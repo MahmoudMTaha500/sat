a6daf8bec9fa04467384a150cda59f61c935f166
mahmoud <mahmoud.m.taha.500@gmail.com>
2021-01-11 12:44:31 +0200

    add folder

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -2,6 +2,29 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('dash_test' , function(){
-    return 'dash test';
+Route::get('/dashboard', function () {
+    $department_name='dashboard';
+    $page_name='dashboard';
+    return view('/admin/dashboard' , compact('department_name' , 'page_name'));
+})->name('dashboard');
+
+
+
+Route::group(['prefix' => 'dashboard'], function() {
+
+  
+
+
+    Route::get('/add-institute', function () {
+        $department_name='institutes';
+        $page_name='add-institute';
+        return view('/admin/institutes.create' , compact('department_name' , 'page_name'));
+    })->name('dashboard.add-institute');
+
+    
+    Route::get("/country","countryController@index");
+
+
 });
+
+
